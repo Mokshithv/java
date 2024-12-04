69ad80cbc79eee4ed8f5408f5d1c512003018ebb
MOKSHITH V <85726740+Mokshithv@users.noreply.github.com>
2024-12-04 07:21:51 +0530

    Update 7.java

--- 7.java
+++ 7.java
@@ -1,35 +1,37 @@
-interface Resizable 
-{ 
-void resizeWidth(int width); 
-void resizeHeight(int height); 
-} 
-class Rectangle implements Resizable 
-{ 
-private int width; 
-private int height; 
-public Rectangle(int width, int height) 
-{ 
-this.width = width; 
-this.height = height; 
-} 
-public void resizeWidth(int newWidth) 
-{ 
-this.width = newWidth; 
-} 
-public void resizeHeight(int newHeight) 
-{ 
-} 
-this.height = newHeight; 
-public void display() 
-{ 
-System.out.println("Rectangle width: " + width + ", height: " + height); 
-} 
-public static void main(String[] args) 
-{ 
-Rectangle rectangle = new Rectangle(5, 10); 
-rectangle.display(); 
-rectangle.resizeWidth(8); 
-rectangle.resizeHeight(15); 
-rectangle.display(); 
-} 
+interface Resizable {
+    void resizeWidth(int width);
+    void resizeHeight(int height);
 }
+
+class Rectangle implements Resizable {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void resizeWidth(int newWidth) {
+        this.width = newWidth;
+    }
+
+    @Override
+    public void resizeHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void display() {
+        System.out.println("Rectangle width: " + width + ", height: " + height);
+    }
+
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 10);
+        rectangle.display();
+        rectangle.resizeWidth(8);
+        rectangle.resizeHeight(15);
+        rectangle.display();
+    }
+}
+
